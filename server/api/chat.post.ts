eb635a9ea57d22503a6d8db16457cc57bc8bd1a1
Pwntus <pontus.edvard@gmail.com>
2023-03-16 13:42:53 +0200

    fix: remove debug

--- server/api/chat.post.ts
+++ server/api/chat.post.ts
@@ -1,6 +1,19 @@
+import { Configuration, OpenAIApi } from 'openai'
+import { createClient } from '@supabase/supabase-js'
 import GPT3Tokenizer from 'gpt3-tokenizer'
 import { OpenAI } from 'openai-streams/node'
 import { sendStream } from 'h3'
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: useRuntimeConfig().openaiApiKey
+  })
+)
+
+const supabase = createClient(
+  useRuntimeConfig().supabaseUrl,
+  useRuntimeConfig().supabaseKey
+)
 
 // @ts-ignore
 const tokenizer = new GPT3Tokenizer.default({ type: 'gpt3' })
@@ -9,7 +22,24 @@ export default defineEventHandler(async (event) => {
   try {
     const { query } = await readBody(event)
 
-    const documents = event.context.documents
+    // OpenAI recommends replacing newlines with spaces for best results
+    const input = query.replace(/\n/g, ' ')
+
+    // Generate a one-time embedding for the query itself
+    const embeddingResponse = await openai.createEmbedding({
+      model: 'text-embedding-ada-002',
+      input
+    })
+
+    const [{ embedding }] = embeddingResponse.data.data
+
+    const { error, data: documents } = await supabase.rpc('match_documents', {
+      query_embedding: embedding,
+      similarity_threshold: 0.8,
+      match_count: 10
+    })
+
+    if (error) return { error: error.message }
 
     // Create context
     let tokenCount = 0
