1846701e9fb670fc162c74d1c8629ecbc95dadca
Pwntus <pontus.edvard@gmail.com>
2023-03-16 07:58:01 +0200

    fix: debug

--- server/api/chat.post.ts
+++ server/api/chat.post.ts
@@ -3,13 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 import GPT3Tokenizer from 'gpt3-tokenizer'
 import { OpenAI } from 'openai-streams/node'
 import { sendStream } from 'h3'
-/*
+
 const openai = new OpenAIApi(
   new Configuration({
     apiKey: useRuntimeConfig().openaiApiKey
   })
 )
-
+/*
 const supabase = createClient(
   useRuntimeConfig().supabaseUrl,
   useRuntimeConfig().supabaseKey
